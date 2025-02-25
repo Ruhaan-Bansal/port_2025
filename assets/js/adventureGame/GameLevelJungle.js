97187bdc86e159087cf77f07ff9e16f7d8168b1a
Ruhaan-Bansal <ruhamobile@gmail.com>
2025-02-24 20:06:33 -0800

    change of pixels to 1024

--- assets/js/adventureGame/GameLevelJungle.js
+++ assets/js/adventureGame/GameLevelJungle.js
@@ -53,7 +53,7 @@ class GameLevelJungle {
         src: sprite_src_panda,
         SCALE_FACTOR: 8,  // Adjust this based on your scaling needs
         ANIMATION_RATE: 50,
-        pixels: {height: 256, width: 352},
+        pixels: {height: 1024, width: 1024},
         INIT_POSITION: { x: (width / 2), y: (height / 2)},
         orientation: {rows: 1, columns: 1 },
         down: {row: 1, start: 0, columns: 1 },  // This is the stationary npc, down is default 
@@ -86,7 +86,7 @@ class GameLevelJungle {
         src: sprite_src_parrot,
         SCALE_FACTOR: 10,  // Adjust this based on your scaling needs
         ANIMATION_RATE: 50,
-        pixels: {height: 301, width: 801},
+        pixels: {height: 1024, width: 1024},
         INIT_POSITION: { x: (width / 4), y: (height / 4)},
         orientation: {rows: 1, columns: 1 },
         down: {row: 1, start: 0, columns: 1 },  // This is the stationary npc, down is default 
@@ -117,7 +117,7 @@ class GameLevelJungle {
         src: sprite_src_monkey,
         SCALE_FACTOR: 10,  // Adjust this based on your scaling needs
         ANIMATION_RATE: 100,
-        pixels: {height: 316, width: 627},
+        pixels: {height: 1024, width: 1024},
         INIT_POSITION: { x: (width * 3 / 4), y: (height * 3 / 4)},
         orientation: {rows: 1, columns: 1 },
         down: {row: 1, start: 0, columns: 1 },  // This is the stationary npc, down is default 
