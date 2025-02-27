60925e292b110a624b501b87639c5934d8a6968a
Ruhaan-Bansal <ruhamobile@gmail.com>
2025-02-27 01:21:00 -0800

    final location of panda

--- assets/js/adventureGame/GameLevelJungle.js
+++ assets/js/adventureGame/GameLevelJungle.js
@@ -55,7 +55,7 @@ class GameLevelJungle {
         SCALE_FACTOR: 5,  // Adjust this based on your scaling needs
         ANIMATION_RATE: 50,
         pixels: {height: 316, width: 627},
-        INIT_POSITION: { x: (width / 2), y: (height / 4)},
+        INIT_POSITION: { x: (width / 2), y: (height * 3/5)},
         orientation: {rows: 3, columns: 6 },
         down: {row: 1, start: 0, columns: 6 },  // This is the stationary npc, down is default 
         hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
@@ -117,7 +117,7 @@ class GameLevelJungle {
         SCALE_FACTOR: 5,  // Adjust this based on your scaling needs
         ANIMATION_RATE: 100,
         pixels: {height: 301, width: 801},
-        INIT_POSITION: { x: (width / 4), y: (height * 1/4)},
+        INIT_POSITION: { x: (width / 4), y: (height * 1/6)},
         orientation: {rows: 1, columns: 4 },
         down: {row: 0, start: 0, columns: 3 },  // This is the stationary npc, down is default 
         hitbox: { widthPercentage: 0.1, heightPercentage: 0.1 },
